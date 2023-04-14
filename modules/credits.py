1ac1494f17463983918cd55a2ee50bc05f009028
Guestwith9 <120591299+Guestwith9@users.noreply.github.com>
2023-04-14 14:36:44 +0000

    Credits now prints out all the data. Data is still not finished.  Fill in the game title placeholder

--- modules/credits.py
+++ modules/credits.py
@@ -1,1 +1,33 @@
-people= {'Dottie Holland': 'Owner', 'DANIEL BOBB': '', 'NATHAN CONNER': '', 'CARLOS CUEVAS', 'JONATHAN CURTIS' :'Programmer', 'CHRISTIAN HOFFA': '', 'ZACHARY HULETT': 'Game Mechanic', 'TAKHYRA JACKSON' : 'Game Mechanic', 'ETHAN MONROE': 'Programmer', 'JONATHAN MORGAN' :'', 'FRANCISCO MOYA' :'', 'JOSHUA MOYA': '', 'KADEN RAMER' :'' 'TYLER RESURRECCION': '', 'BENJAMIN SALINAS': '', 'ADDISON SCHEER': 'Progammer', 'BARRY SCHIEBER': '', 'CHRISTOPHER SIMPSON-HANSON': 'Game Mechanic', 'DAVID SKIPP': '', 'ELIJAH STOKES': '', 'NICHOLAS TOUMA': '', 'DYLAN WAGONER': '', 'ANTHONY WITTIC', ''}
+import time
+people= {'Dottie Holland': 'Owner',
+'DANIEL BOBB': '',
+'NATHAN CONNER': '',
+'CARLOS CUEVAS': '',
+'JONATHAN CURTIS': 'Programmer',
+'CHRISTIAN HOFFA': '', 
+'ZACHARY HULETT': 'Game Mechanic',
+'TAKHYRA JACKSON' : 'Game Mechanic', 
+'Spencer Lin': 'Programmer',
+'ETHAN MONROE': 'Programmer', 
+'JONATHAN MORGAN' :'', 
+'FRANCISCO MOYA' :'',
+'JOSHUA MOYA': '', 
+'KADEN RAMER' :'',
+'TYLER RESURRECCION': '', 
+'BENJAMIN SALINAS': '', 
+'ADDISON SCHEER': 'Progammer', 
+'BARRY SCHIEBER': '', 
+'CHRISTOPHER SIMPSON-HANSON': 'Game Mechanic', 
+'DAVID SKIPP': '', 
+'ELIJAH STOKES': '', 
+'NICHOLAS TOUMA': '', 
+'DYLAN WAGONER': '', 
+'ANTHONY WITTIC': ''}
+key= list(people.keys())
+print('UNCURED credits:')
+time.sleep(2)
+for i in range(len(people)):
+    time.sleep()
+    print(f'{key[i]} - {people.get(key[i])}')
+
+    
