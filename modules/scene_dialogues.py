150a828c637c04d0c5e83778988fd1a0f93b1ffd
Guestwith9 <120591299+Guestwith9@users.noreply.github.com>
2023-04-20 09:19:03 -0500

    working on it

--- modules/scene_dialogues.py
+++ modules/scene_dialogues.py
@@ -1,7 +1,31 @@
 import time
-def dis_dia(dialogue, delay):
+def p(dialogue, delay):
   time.sleep(delay)
-  print(dialogue)
-  return dialogue
+  print(dialgoue, '...')
+  return p
 
 dis_dia('test', 2)
+
+
+p('A helicopter lands on a hospital', 1)
+p('Stepping out of the helicopter is you, a government agent', 3)
+p('You are tasked and debriefed to an asteroid that crashed a couple days ago to study a rapidly spreading virus', 5)
+p('Your helicopter lands at the hospital helipad', 3)
+p('You decend to the bathroom floor but find all the roof doors are locked', 5)
+"""
+Section 1 and 2
+"""
+p('You have now reached the crushed site', 2)
+p('You collect and chip away pieces of the asteroid to bring back to the base', 4)
+p('But as you collect the samples, the asteriod begins to crack', 3)
+p('You step back and watch it crumble', 2)
+p('All of a sudden, your hazard suit warns you a spike of radiation', 5)
+p('The asteroid is going to explode',3)
+p('You call for extraction and are told to fend off the now alerted uncured until they can resue you.', 7)
+
+"""
+Fight off the uncured zombies
+"""
+
+p('You successfully fend off the uncured horde', 3)
+p('A helicopter flies above you and descends a rope for you to climb', 6)
