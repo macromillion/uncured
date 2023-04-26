110d4b4691b11412dd318166463a8194e437966b
Guestwith9 <120591299+Guestwith9@users.noreply.github.com>
2023-04-26 09:08:41 -0500

    Update scene_dialogues.py

--- modules/scene_dialogues.py
+++ modules/scene_dialogues.py
@@ -1,41 +1,44 @@
 import time
 def p(dialogue, delay):
+  if dialogue[-1] != '!':
+    print(dialogue + '...')
+  else:
+    print(dialogue)
   time.sleep(delay)
-  print(dialogue, '...')
   return p
 
-p('A helicopter lands on a hospital', 1)
+p('A helicopter lands on a hospital', 2)
 p('Stepping out of the helicopter is you, a government agent', 3)
 p('You are tasked and debriefed to an asteroid that crashed a couple days ago to study a rapidly spreading virus', 5)
-p('Your helicopter lands at the hospital helipad', 3)
-p('You decend to the floor below but find all the roof doors locked', 5)
+p('Your helicopter lands at the hospital helipad', 2.5)
+p('You decend to the floor below but find all the roof doors locked', 3.2)
 """
 Section 1 and 2
 """
 p('You have now reached the crush site', 2)
-p('You collect and chip away pieces of the asteroid to bring back to the base', 4)
+p('You collect and chip away pieces of the asteroid to bring back to the base', 3.5)
 p('But as you collect the samples, the asteriod begins to crack', 3)
-p('You step back and watch it crumble', 2)
-p('All of a sudden, your hazard suit warns you a spike of radiation', 5)
-p('The asteroid is going to explode',3)
-p('You call for extraction and are told to fend off the now alerted uncured until they can resue you.', 7)
+p('You step back and watch it crumble', 2.2)
+p('All of a sudden, your hazard suit warns you a spike of radiation', 3.3)
+p('The asteroid is going to explode',2.5)
+p('You call for extraction and are told to fend off the now alerted uncured until they can resue you.', 5)
 
 """
 Fight off the uncured zombies
 """
 
-p('You successfully fend off the uncured horde', 3)
-p('A helicopter flies above you and sends down a rope for you to climb', 6)
-p('As you fly back to your base, the asteroid explodes', 3)
+p('You successfully fend off the uncured horde', 2.5)
+p('A helicopter flies above you and sends down a rope for you to climb', 3.5)
+p('As you fly back to your base, the asteroid explodes', 2.7)
 """
 more descriptions
 """
-p('It sends out a shockwave across the world', 3)
+p('It sends out a shockwave across the world', 2.5)
 p('You arrive at the base', 2)
-p('General: '+ playername +', our scientists are unable to find a cure for this new virus', 5)
-p("General: It's beyond our technolgoies", 3)
-p('General: However, over at our Mars base, we have the equipment to help find the cure', 5)
-p('General: We have received word an asteroid from the same field the one we got struck-',1)
+p('General: '+ playername +', our scientists are unable to find a cure for this new virus', 4.5)
+p("General: It's beyond our technology", 2)
+p('General: However, over at our Mars base, we have the equipment to help find a cure', 4.5)
+p('General: We have received word an asteroid from the same field the one we got struck-',4)
+p('Boom!', 0.5)
 p('Boom! An explosion shakes the base', 3)
 p('The ground crumbles and a sinkhole appears', 4)
-
